b43d511de788ba2e06ec0dda9fb899182a6370b2
Dawid4678 <Dawid4678>
2023-06-12 19:24:07 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.setProperty("WebDriver.Chrome.Driver","C:\\Users\\daw-s\\Desktop\\repozytoria\\Selenium\\ChromeDriver\\chromedriver.exe");
+        ChromeOptions ops = new ChromeOptions();
+        ops.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(ops);
+        driver.get("https://www.w3schools.com/xml/xpath_syntax.asp");
+        //*[@id="accept-choices"]
+        driver.findElement(By.xpath("//*[@id=\"accept-choices\"]")).click();
     }
 }
